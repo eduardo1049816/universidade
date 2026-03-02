309f3126a19faa5ab92a103e836701b65b662ccf
Eduardo Ferraz Coutinho Soares Ferreira <ferrazeduardo739@gmail.com>
2026-03-02 14:30:12 -0300

    alteraçoes no codigo

---
+++ python/screen/cadastrar.py
@@ -0,0 +1,13 @@
+from PySide6.QtWidgets import QApplication
+
+from screen.cadastrar import Cadastrar
+
+import sys
+
+if __name__ =="__main__":
+    app = QApplication(sys)
+
+    tela = Cadastrar(app)
+    tela.janela.show()
+
+    sys.exit(tela.app.exec())
